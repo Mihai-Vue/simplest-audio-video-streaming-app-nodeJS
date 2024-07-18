1d05cfeeae5ade07a4d889cc14c5eb0ed346d1e3
Mihai Boureanu <boureanumihai@yahoo.com>
2024-07-18 07:48:36 +0300

    trimming code

--- http.js
+++ http.js
@@ -5,6 +5,5 @@ const audio = require("./routes/audio");
 const port = 3000;
 
 app.use("/api/v1/audio", audio);
-app.use("/api/v1/audio/", audio);
 
 app.listen(port, console.log(`server is listening on port: ${port}`));
